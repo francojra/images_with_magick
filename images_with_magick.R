7bac955bc295534a7794055d2560bbf2ffb4f441
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-08-04 19:22:32 -0300

    Add files via upload

--- images_with_magick.R
+++ images_with_magick.R
@@ -20,3 +20,23 @@ image_scale(ave, "400")
 image_blur(ave, 10, 5)
 
 image_charcoal(ave)
+
+image_oilpaint(ave)
+
+image_negate(ave)
+
+image_flatten(ave, 'Add')
+
+image_flatten(ave, 'Modulate')
+
+## Adicionando texto
+
+image_annotate(ave, "Augastes lumachella \n Beija-flor-de-gravata-vermelha",
+               size = 20,
+               gravity = "southwest",
+               color = "black")
+
+## Adicionando borda e plano de fundo
+
+image_background(ave, "blue", flatten = TRUE)
+test <- image_border(test, "red", "10x10")
